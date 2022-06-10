544231750e9a24b7d50f31a354fc84543b2b5e74
Lohana Torres <lt.leopoldo@gmail.com>
2022-06-09 23:53:02 -0300

    refactor: remove unnecessary responsibilities from routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,16 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/categories-repository";
-import { CreateCategoryService } from "../modules/cars/services/create-category-service";
+import { createCategoryController } from "../modules/cars/usecases/create-category";
+import { listCategoriesController } from "../modules/cars/usecases/list-categories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-  new CreateCategoryService(categoriesRepository).execute({
-    name,
-    description,
-  });
-  return response
-    .status(201)
-    .json({ message: "Category created successfully." });
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-  const all = categoriesRepository.list();
-  return response.json(all);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
