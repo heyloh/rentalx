fcb33fa31c43fd5e953f0408ff2966d165702d73
Lohana Torres <lt.leopoldo@gmail.com>
2022-06-10 00:16:49 -0300

    refactor: isolating routes from server setup

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import express from "express";
 
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import { router } from "./routes";
 
 // Define the application port
 const port = 3333;
@@ -11,10 +10,7 @@ const app = express();
 
 // Use JSON middleware to parse the body of the request
 app.use(express.json());
-
-// Routes
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use(router);
 
 // Start server
 app.listen(port, () =>
