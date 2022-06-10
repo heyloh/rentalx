f8924e368a2694d4558a63c48ef19def8507f872
Lohana Torres <lt.leopoldo@gmail.com>
2022-06-09 22:34:03 -0300

    feat: create category model

---
+++ src/modules/cars/models/category.ts
@@ -0,0 +1,21 @@
+import { v4 as uuidv4 } from "uuid";
+
+class Category {
+  id?: string;
+  name: string;
+  description: string;
+  created_at?: Date;
+
+  constructor(name: string, description: string) {
+    this.name = name;
+    this.description = description;
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+    if (!this.created_at) {
+      this.created_at = new Date();
+    }
+  }
+}
+
+export { Category };
