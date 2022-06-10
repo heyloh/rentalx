c5974ed09a2b48bfaf867f27e0d06420578606da
Lohana Torres <lt.leopoldo@gmail.com>
2022-06-09 23:49:56 -0300

    refactor: create index for create category controller

---
+++ src/modules/cars/usecases/create-category/index.ts
@@ -0,0 +1,11 @@
+import { CategoriesRepository } from "../../repositories/categories-repository";
+import { CreateCategoryController } from "./create-category-controller";
+import { CreateCategoryUseCase } from "./create-category-usecase";
+
+const categoriesRepository = CategoriesRepository.getInstance();
+const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
+const createCategoryController = new CreateCategoryController(
+  createCategoryUseCase
+);
+
+export { createCategoryController };
