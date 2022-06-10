d4285273a9b417d44f2c88d54d532905af33696b
Lohana Torres <lt.leopoldo@gmail.com>
2022-06-10 00:13:02 -0300

    refactor: remove unnecessary responsibilities from routes

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,26 +1,16 @@
 import { Router } from "express";
 
-import { SpecificationsRepository } from "../modules/cars/repositories/specifications-repository";
-import { CreateSpecificationService } from "../modules/cars/services/create-specification-service";
+import { createSpecificationController } from "../modules/cars/usecases/create-specification";
+import { listSpecificationsController } from "../modules/cars/usecases/list-specifications";
 
 const specificationsRoutes = Router();
 
-const specificationsRepository = new SpecificationsRepository();
-
 specificationsRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-  new CreateSpecificationService(specificationsRepository).execute({
-    name,
-    description,
-  });
-  return response
-    .status(201)
-    .json({ message: "Specification created successfully." });
+  return createSpecificationController.handle(request, response);
 });
 
 specificationsRoutes.get("/", (request, response) => {
-  const all = specificationsRepository.list();
-  return response.status(200).json(all);
+  return listSpecificationsController.handle(request, response);
 });
 
 export { specificationsRoutes };
